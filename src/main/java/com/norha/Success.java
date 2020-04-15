6fec499d7c83e5b09c3cdbb942a16ff5f2727bdf
Robinclear <robin.peter.vkm@gmail.com>
2020-04-14 23:59:05 -0300

    Added User Pojo And Populated the vlaues

--- src/main/java/com/norha/Success.java
+++ src/main/java/com/norha/Success.java
@@ -11,7 +11,9 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
+import com.norha.model.GoogleToken;
 import com.norha.model.RequestTokenParams;
+import com.norha.model.User;
 
 /**
  * Servlet implementation class Success
@@ -20,26 +22,38 @@ import com.norha.model.RequestTokenParams;
 public class Success extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static Client client = ClientBuilder.newClient();
-	WebTarget googleTokenServer = client.target("https://www.googleapis.com/oauth2/v4/token"); 
+	WebTarget googleTokenServer = client.target("https://www.googleapis.com/oauth2/v4/token");
 
-    public Success() {
-        super();
-    }
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// step 2
 		String code = request.getParameter("code");
 		String client_id = "516880447905-lqhjlpig4oea5b5rt1rcomf5k2lr5ao9.apps.googleusercontent.com";
 		String client_secret = "AyYlIbAp5aDfMz4b-jjwt23f";
-		String redirect_uri = "http://localhost:8080/S07-GoogleOauth/success";	
+		String redirect_uri = "http://localhost:8080/S07-GoogleOauth/success";
 		String grant_type = "authorization_code";
+		RequestTokenParams requestTokenParams = new RequestTokenParams(code, client_id, client_secret, redirect_uri,
+				grant_type);
+		Response tokenInJson = googleTokenServer.request().post(Entity.json(requestTokenParams));
+		GoogleToken googleToken = tokenInJson.readEntity(GoogleToken.class);
+		// step 3
+		WebTarget googleUserInfoAPI = client
+				.target("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + googleToken.getAccess_token());
+		Response userInfo = googleUserInfoAPI.request().get();
+		User user = userInfo.readEntity(User.class);
 		
-		RequestTokenParams requestTokenParams = new RequestTokenParams(code, client_id, client_secret, redirect_uri, grant_type);
-		Response token = googleTokenServer.request().post(Entity.json(requestTokenParams));
-		System.out.println("Token = "+token.readEntity(String.class));
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println(user);
 	}
-	
-	/*Got from the google Login
+
+	/*
+	 * 
+	 * Google API URL (OPENID) URL: https://www.googleapis.com/oauth2/v2/userinfo
+	 * 
+	 * Submit the “access_token” as a GET parameter to get user information
+	 */
+
+	/*
+	 * Got from the google Login
 	 * http://localhost:8080/S07-GoogleOauth/success?code=4%
 	 * 2FygEVDjWG2XJ5oemsKbXSXnkoI79GymVP2aMJLXsqMj6citEzlduuIi40fjfkS5iogBZIWXMRPYSUKkJqht2QRqE
 	 * &scope=email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2>Hello33333333333 World!</h2>
-
+<!-- //step 1  -->
 <!-- <a href="https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri={Your Redirect URL}&response_type=code&client_id={Your Client ID}&approval_prompt=force"></a> -->
 <a href="https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8080/S07-GoogleOauth/success&response_type=code&client_id=516880447905-lqhjlpig4oea5b5rt1rcomf5k2lr5ao9.apps.googleusercontent.com&approval_prompt=force">
 Google Sign In</a>
