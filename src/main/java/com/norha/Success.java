3977c1db385b0adc931a6cb8f42274850f0581f2
Robinclear <robin.peter.vkm@gmail.com>
2020-04-15 00:06:32 -0300

    created the displayUserInfo.jsp
    and requestDespatch to it with User data

--- src/main/java/com/norha/Success.java
+++ src/main/java/com/norha/Success.java
@@ -41,7 +41,8 @@ public class Success extends HttpServlet {
 				.target("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + googleToken.getAccess_token());
 		Response userInfo = googleUserInfoAPI.request().get();
 		User user = userInfo.readEntity(User.class);
-		
+		request.setAttribute("User", user);
+		request.getRequestDispatcher("displayUserInfo.jsp").forward(request, response);
 		System.out.println(user);
 	}
 
