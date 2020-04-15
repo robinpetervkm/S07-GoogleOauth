f7d199b7fcda7354ea6b9be69d9c2c07c557fe34
Robinclear <robin.peter.vkm@gmail.com>
2020-04-14 22:54:16 -0300

    already Added the jersey deplendancy
    jersey bundle , jersey-media-moxy,jaxrs-ri

--- src/main/java/com/norha/Success.java
+++ src/main/java/com/norha/Success.java
@@ -18,7 +18,9 @@ public class Success extends HttpServlet {
     }
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String code = request.getParameter("code");
+		System.out.println(code);
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 	
